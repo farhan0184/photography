b5397c733daa482e63251603d4481d2c6a0bbcfe
farhan0184 <shahariar0184@gmail.com>
2024-07-06 23:35:58 +0600

    protography project first commit add shadcn, framer motion, swiper, countup icons

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ThemeToggler from './ThemeToggler'
+
+export default function Header() {
+  return (
+    <div>
+        <header>
+            <ThemeToggler />
+        </header>
+    </div>
+  )
+}
